9acd68a44c25d615792a7fd0cecf32b4e6ab7f02
Akhmad Rizki Prayoga <akhmadrizki24@gmail.com>
2020-11-29 17:33:57 +0800

    make cards style in main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -17,22 +17,23 @@ const Test = ({navigation}) => {
             <View style={styles.content}>
                 <Text style={{color: '#fff', textAlign: 'center', fontSize: 18, maxWidth: 250 }}>Cari Dan Booking Ruangan Yang Ingin Anda Pinjam</Text>
             </View>
-            <View style={styles.wrapper}>
+            {/* <View style={styles.wrapper}>
                 <Text style={{color: colors.dark, fontSize: 14}}>List Ruangan</Text>
                 <View style={{ marginTop: 12, backgroundColor: '#fff', borderRadius: 8 }}>
                     <Image source={kelas} style={styles.illustration} />
+                    <Text style={{ flexDirection: 'row' }}>Text</Text>
                 </View>
-                <View style={{ marginTop: 12, backgroundColor: '#fff', borderRadius: 8 }}>
-                    <Image source={kelas} style={styles.illustration} />
-                </View>
-                <View style={{ marginTop: 12, backgroundColor: '#fff', borderRadius: 8 }}>
-                    <Image source={kelas} style={styles.illustration} />
-                </View>
-                <View style={{ marginTop: 12, backgroundColor: '#fff', borderRadius: 8 }}>
-                    <Image source={kelas} style={styles.illustration} />
-                </View>
-                <View style={{ marginTop: 12, backgroundColor: '#fff', borderRadius: 8 }}>
-                    <Image source={kelas} style={styles.illustration} />
+            </View> */}
+
+            <View style={styles.cardsWrapper}>
+                <Text style={{color: '#333'}}>List Ruangan</Text>
+                <View style={styles.card}>
+                    <View style={styles.cardImgWrapper}>
+                        <Image source={kelas} resizeMode="cover" style={styles.cardImg} />
+                    </View>
+                    <View style={styles.cardInfo}>
+                        <Text style={styles.cardTitle}>Ruang 3A</Text>
+                    </View>
                 </View>
             </View>
         </ScrollView>
@@ -46,9 +47,9 @@ function Main() {
 
       <Tab.Screen name="Home" component={Test}
       options={{
-        tabBarLabel: 'Updates',
+        tabBarLabel: 'Beranda',
         tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="car-off" color={color} size={26} />
+          <MaterialCommunityIcons name="home" color={color} size={26} />
         ),
       }}/>
 
@@ -59,6 +60,51 @@ function Main() {
 }
 
 const styles = {
+    cardsWrapper: {
+        marginTop: 20,
+        width: '90%',
+        alignSelf: 'center',
+      },
+      card: {
+        height: 100,
+        marginVertical: 10,
+        flexDirection: 'row',
+        shadowColor: '#999',
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5,
+      },
+      cardImgWrapper: {
+        flex: 1,
+      },
+      cardImg: {
+        height: '100%',
+        width: '100%',
+        alignSelf: 'center',
+        borderRadius: 8,
+        borderBottomRightRadius: 0,
+        borderTopRightRadius: 0,
+      },
+      cardInfo: {
+        flex: 2,
+        padding: 10,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderLeftWidth: 0,
+        borderBottomRightRadius: 8,
+        borderTopRightRadius: 8,
+        backgroundColor: '#fff',
+      },
+      cardTitle: {
+        fontWeight: 'bold',
+      },
+      cardDetails: {
+        fontSize: 12,
+        color: '#444',
+      },
+
+
     content: {
         backgroundColor: colors.default,
         paddingVertical: 60,
@@ -73,7 +119,8 @@ const styles = {
     illustration: {
         width: 115, 
         height: 115,
-        borderRadius: 8
+        borderRadius: 8,
+        flexDirection: 'row'
     }
 }
 
